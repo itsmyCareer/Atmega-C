b76416e34a26bac5dba00ecefd0cf187af6bbd1f
ItsmyCareer <itsntmysurport@gmail.com>
2020-06-20 15:37:20 +0900

    INIT: PROJECT

---
+++ MY/main.c
@@ -0,0 +1,18 @@
+/*
+ * MY.c
+ *
+ * Created: 2020-06-20 오후 3:33:08
+ * Author : itsnt
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
