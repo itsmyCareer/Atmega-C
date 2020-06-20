25d68eb90c8eb56657ac81c8654e8781c33f4a8e
ItsmyCareer <itsntmysurport@gmail.com>
2020-06-20 16:50:00 +0900

    Version 1

--- MY/main.c
+++ MY/main.c
@@ -1,5 +1,6 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 #include "MyBluetooth.h"
 #include "MYDC.h"
 #include "MYServo.h"
@@ -7,27 +8,43 @@
 int main(void)
 {
 	char rx_txt;
+	int cnt = 125, toggle = 0;
 	bluetooth_Init();
 	DC_INIT();
 	Servo_INIT();
-    while (1) 
+
+	while (1) 
     {
 		rx_txt = bluetooth_Rx();
 		switch(rx_txt)
 		{
 			case "1":
 				DC_Motor(30);
+				toggle = 1;
 				break;
 			case "2":
 				DC_Motor(60);
+				toggle = 2;
 				break;
 			case "3":
 				DC_Motor(100);
+				toggle = 3;
 				break;
 			case "4":
+				toggle = 4;
 				break;
 		}
-
+		
+		while(!(UCSR1A & 0x80))
+		{
+			if(toggle != 4)
+			{
+				Servo_Motor(cnt++);
+				_delay_ms(4);
+				if(cnt > 725)	cnt = 125;
+			}
+		}
     }
+
 }
 
